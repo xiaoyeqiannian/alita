d8edea78cf8f4a60a3e3caa5972ff948e872fc27
ck <xiaoyeqiannian@163.com>
2020-04-13 19:46:13 +0800

    fixed some config

--- wsgi_dev.py
+++ wsgi_dev.py
@@ -3,6 +3,6 @@
 import os
 import inc
 
-config_file = os.environ.get("ALITA_CONFIG", "config/dev/dev.py")
+config_file = os.environ.get("ALITA_CONFIG", "../config/dev/dev.py")
 application = inc.create_app(config_file)
 
