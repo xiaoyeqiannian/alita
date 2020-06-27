781dff29c8cdfc6ef2fbb7277f541d28064feb45
ck <xiaoyeqiannian@163.com>
2020-06-27 09:52:17 +0800

    add wxdb aggregate

--- util/wx_api.py
+++ util/wx_api.py
@@ -27,6 +27,7 @@ class WeChatCloudDB(object):
         self.query = ''
         self.url = ''
         self.method = ''
+        self.total = 0
 
     def paginate(self, page=None, per_page=None, max_per_page=100):
         if page is None:
@@ -44,18 +45,33 @@ class WeChatCloudDB(object):
         if per_page < 0:
             per_page = 20
 
-        self.query += '.limit({0}).skip({1})'.format(per_page, (page-1)*per_page)
+        if self.method == 'aggregate' and not self.total:
+            tmp_query = self.query
+            self.query += '.count("trade_count")'
+            ret = self.commit()
+            if ret:
+                trade_count = json.loads(ret[0])['trade_count']
+                self.total = self.parseNumber(trade_count)
+            self.query = tmp_query
+            
+        self.query += '.skip({1}).limit({0})'.format(per_page, (page-1)*per_page)
         items = self.commit()
         return Pagination(self, page, per_page, self.total, items)
 
     def commit(self):
         url = self.url + '?' + self.access_token
-        data = {'env': self.env, 'query': self.query+'.get()' if self.method=='get' else self.query}
+        query_type = ''
+        if self.method == 'get':
+            query_type = '.get()'
+        elif self.method == 'aggregate':
+            query_type = '.end()'
+
+        data = {'env': self.env, 'query': self.query + query_type}
         response = requests.post(url, data=json.dumps(data))
         results = json.loads(response.text)
         if results and results.get('errcode')==0:
             pager = results.get('pager')
-            if pager:
+            if pager and pager.get('Total'):
                 self.total = pager.get('Total')
             if results.get('data'):
                 return results.get('data')
@@ -71,7 +87,7 @@ class WeChatCloudDB(object):
         else:
             print(results.get('errcode'), results.get('errmsg'))
             return []
-        
+
     def get(self, table, _id, **kwargs):
         self.method = 'get'
         self.url = 'https://api.weixin.qq.com/tcb/databasequery'
@@ -79,16 +95,55 @@ class WeChatCloudDB(object):
         if _id:
             self.query += '.doc("{}")'.format(_id)
         elif kwargs:
-            filter_str = ''
+            filter_list = []
             for k,v in kwargs.items():
-                if isinstance(v, str):
-                    filter_str += '{0}:"{1}"'.format(k, v)
-                elif isinstance(v, bool):
-                    filter_str += '{0}:{1}'.format(k, v and 'true' or 'false')
-                else:
-                    filter_str += '{0}:{1}'.format(k, v)
+                if k == 'orderby':
+                    for i in v:
+                        self.query += '.orderBy("{0}","{1}")'.format(i[0], i[1])
+                    continue
 
+                if isinstance(v, str):
+                    filter_list.append('{0}:"{1}"'.format(k, v))
+                elif isinstance(v, bool):
+                    filter_list.append('{0}:{1}'.format(k, v and 'true' or 'false'))
+                else:
+                    filter_list.append('{0}:{1}'.format(k, v))
+
+            filter_str = ','.join(filter_list)
             self.query += '.where({%s})' % filter_str
+        return self
+    
+    def aggregate(self, table, lookups, **kwargs):
+        self.method = 'aggregate'
+        self.url = 'https://api.weixin.qq.com/tcb/databaseaggregate'
+        self.query = 'db.collection("{}").aggregate()'.format(table)
+        if lookups:
+            for lookup in lookups:
+                lookup_list = []
+                for k,v in lookup.items():
+                    lookup_list.append('{0}:"{1}"'.format(k, v))
+                lookup_str = ','.join(lookup_list)
+                self.query += '.lookup({%s})' % lookup_str
+
+        if kwargs:
+            filter_list = []
+            for k,v in kwargs.items():
+                if k == 'orderby':
+                    sort_list = []
+                    for i in v:
+                        sort_list.append("{0}:{1}".format(i[0], i[1]))
+                    sort_str = ','.join(sort_list)
+                    self.query += '.sort({%s})' % sort_str
+                    continue
+
+                if isinstance(v, str):
+                    filter_list.append('{0}:"{1}"'.format(k, v))
+                elif isinstance(v, bool):
+                    filter_list.append('{0}:{1}'.format(k, v and 'true' or 'false'))
+                else:
+                    filter_list.append('{0}:{1}'.format(k, v))
+            filter_str = ','.join(filter_list)
+            self.query += '.match({%s})' % filter_str
         return self
    
     def update_one(self, table, _id, **kwargs):
@@ -148,6 +203,24 @@ class WeChatCloudDB(object):
             self.query += '.where({%s})' % filter_str
         self.query += '.count()' 
         return self
+
+
+    @classmethod
+    def parseNumber(cls, data):
+        if not isinstance(data, dict):
+            return data
+        try:
+            (_type, v) = [i for i in data.items()][0]
+        except:
+            return 0
+ 
+        if _type == '$numberDouble':
+            return float(v)
+
+        if _type == '$numberInt':
+            return int(v)
+
+        return 0
 
 
 class WeChatCloudFile(object):
